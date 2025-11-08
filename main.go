3e8f41b3e117e0275b122cc580d389dba2cc9604
Moee1149 <shahi.kshitij55@gmail.com>
2025-11-08 21:05:07 +0545

    metrics route added

--- main.go
+++ main.go
@@ -3,7 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetrics(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -12,11 +25,19 @@ func main() {
 		Handler: mux,
 	}
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	afgConfig := apiConfig{}
+
+	mux.Handle("/app/", http.StripPrefix("/app", afgConfig.middlewareMetrics(http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
+	})
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		hits := afgConfig.fileServerHits.Load()
+		w.Write([]byte("Hits: " + strconv.FormatInt(int64(hits), 10)))
 	})
 
 	err := server.ListenAndServe()
